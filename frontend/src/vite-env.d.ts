00f688bdd1804198ebfc21d2d6b424db0c85e0d7
Krishna-Sai <saikrishnagokam@gmail.com>
2025-07-08 15:58:05 +0530

    Update vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: "https://webscrapping-uol6.onrender.com/api" | 'http://localhost:5000/api';
+  // more env variables...
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
